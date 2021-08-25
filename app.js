467a4b6b0206817f5a4d73cc203161c6d405db1c
Zahir <H113800@acsicorp.com>
2021-08-25 15:05:47 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Very Excited for GitHub Repository!'));
+app.get('/', (req, res) => res.send('Test CI/CD Pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
